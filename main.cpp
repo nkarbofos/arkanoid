4e73c39a53d780370f76db7833713f3cdf6824ad
Nikita <nkarbofos@gmail.com>
2017-11-12 18:42:52 +0300

    сделал ускорение

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main()
     window.setFramerateLimit(60);
     sf::Event event;
     sf::Texture l_texture, b_texture[3], bl_texture;
-    l_texture.loadFromFile("line.png");
+    l_texture.loadFromFile("bigline.png");
     b_texture[0].loadFromFile("brick-blue.png");
     b_texture[1].loadFromFile("brick-green.png");
     b_texture[2].loadFromFile("brick-orange.png");
@@ -32,10 +32,14 @@ int main()
     l_sprite.setPosition(x_pos, 500);
     int b_x_pos = 450;
     int b_y_pos = 300;
+    float y_speed = 1;
 
     int b_x = -20;
     int b_y = 100;
     int i = 0;
+
+    bool b_life[63];
+    for(int i = 0; i < 63; i++) b_life[i] = true;
     for(int y = 0; y < 3; y++)
     {
         for(int x = 0; x < 21; x++)
@@ -52,6 +56,7 @@ int main()
 
     int x_direction = 0;
     int y_direction = 4;
+
     while(window.isOpen())
     {
         window.clear();
@@ -65,26 +70,50 @@ int main()
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
             if(x_pos > 0)
-                x_pos -= 4;
+                x_pos -= 7;
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-            if(x_pos < 700)
-                x_pos += 4;
+            if(x_pos < 750)
+                x_pos += 7;
         l_sprite.setPosition(x_pos, 500);
         window.draw(l_sprite);
-        for(int i = 0; i < 63; i++) window.draw(bricks[i]);
+        for(int i = 0; i < 63; i++) if(b_life[i] == true) window.draw(bricks[i]);
+        else bricks[i].setPosition(1000, 1000);
 
-        if(bl_sprite.getGlobalBounds().intersects(l_sprite.getGlobalBounds()))
+        if(bl_sprite.getGlobalBounds().intersects(l_sprite.getGlobalBounds()) || b_y_pos >580)
         {
             y_direction = -4;
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) x_direction = -4;
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) x_direction = 4;
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) x_direction -= 1;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) x_direction += 1;
         }
-        for(int i = 0; i < 63; i++) if(bl_sprite.getGlobalBounds().intersects(bricks[i].getGlobalBounds())) y_direction = 4;
 
+        int change_direction = 1;
+
+        for(int i = 0; i < 63; i++)
+        {
+            if(b_life[i] && bl_sprite.getGlobalBounds().intersects(bricks[i].getGlobalBounds()))
+            {
+                change_direction = -1;
+                b_life[i] = false;
+            }
+        }
+
+        if(b_y_pos < 0)
+        {
+            change_direction = -1;
+
+        }
+
+        y_direction *= change_direction;
         if(b_x_pos < 0 || b_x_pos > 780) x_direction -= x_direction * 2;
+    //    if(last_bl_speed == 60)
+  //      {
+//            y_direction -= change_direction;
+      //      last_bl_speed = 0;
+       // }
         b_x_pos += x_direction;
-        b_y_pos += y_direction;
+        b_y_pos += y_direction * y_speed;
         bl_sprite.setPosition(b_x_pos, b_y_pos);
+        y_speed += 0.0001;
         window.draw(bl_sprite);
         window.display();
     }
